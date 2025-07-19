d7c174721484f2b80b3b81cde126e6c17e32fba2
Evin <evinwoon@gmail.com>
2025-07-19 10:18:08 +0200

    updated vite config file

--- music-maker/vite.config.js
+++ music-maker/vite.config.js
@@ -1,3 +1,10 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/musicMaker.js/",
-};
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+});
